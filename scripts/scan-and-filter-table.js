f8543853904a15450c7ac87de8124bd40b4b6788
tsalm0n <tsalm0n@twc.com>
2019-01-14 09:52:21 -0500

    searches enabled for any number or combination of form fields being filled out in search form

--- scripts/scan-and-filter-table.js
+++ scripts/scan-and-filter-table.js
@@ -19,6 +19,8 @@ app.post('/api/data', (request, response) => {
 
     //console.log(Object.values(postBody)[0]);
 
+
+
     /**scan and filter table******************************************************************************** */
     var AWS = require('aws-sdk');
     var dyn = new AWS.DynamoDB({
@@ -27,90 +29,94 @@ app.post('/api/data', (request, response) => {
         accessKeyId: 'DEFAULT_ACCESS_KEY', // needed if you don't have aws credentials at all in env
         secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
     });
-/*
-    for (n = 0; n < 2; n++) {
-        if (Object.values(postBody)[n] == null) {
-            return ;
-        } else {
-            scanFilter();
+
+    var filterExpArray = []; // base array that holds POST data (input from html form) for FilterExpression
+
+    var filterExpString = []; //one element array that holds string for FilterExpression
+
+    var lname_exp;
+    var fname_exp;
+
+    if (postBody["lname"] !== '') { //if data input in html form not empty, push string for FilterExpression to filterExpArray
+        lname_exp = "#last_name = :lname";
+        filterExpArray.push(lname_exp);
+    };
+    if (postBody["fname"] !== '') {
+        fname_exp = "#first_name = :fname";
+        filterExpArray.push(fname_exp);
+    };
+
+    console.log('filterExpArray.length =' + filterExpArray.length)
+
+    if (filterExpArray.length > 1) {
+        for (n = 0; n < (filterExpArray.length - 1); n++) {
+            //add " AND " to each element in array, exept last
+            filterExpArray.splice(n, 1, (filterExpArray[n] + " AND "));
         }
     }
-*/
-/*
 
-var obj = {
-    key1: value1,
-    key2: value2
-};
+    console.log('filterExpArray = ' + filterExpArray);
 
-var getProperty = function (propertyName) {
-    return obj[propertyName];
-};
+    function filterExpFunc() {
+        if (filterExpArray.length == 1) {
+            var fE = filterExpArray[0];
+            filterExpString.push(fE);
+        } else {
+            if (filterExpArray.length > 1) {
+                for (m = 0; m < filterExpArray.length - 1; m++) {
+                    var fE = filterExpArray[m] += filterExpArray[m + 1];
+                    filterExpString.push(fE)
+                }
+            }
+        }
 
-obj["key3"] = "value3";
+        console.log('fE = ' + fE);
+        console.log('filterExpString = ' + filterExpString);
+    }
 
-getProperty("key1");
-getProperty("key2");
-getProperty("key3");
-*/
+    filterExpFunc();
+    //console.log('filterExp = '+fE);
 
-/*
-
-var obj = {
-    key1: value1,
-    key2: value2
-};
-
-var getProperty = function (propertyName) {
-    return obj[propertyName];
-};
-
-obj["key3"] = "value3";
-
-getProperty("key1");
-getProperty("key2");
-getProperty("key3");
-*/
-
-/*
-params["ExpressionAttributeNames"] = {}
-*/
     {
         var params = {
             TableName: 'teamster-application-database',
             /* required */
             ExpressionAttributeNames: {
-                "#last_name": Object.keys(postBody)[0], //lname
-                "#first_name": Object.keys(postBody)[1] //fname
+                //"#last_name": Object.keys(postBody)[0], //lname
+                //"#first_name": Object.keys(postBody)[1] //fname
             },
             ExpressionAttributeValues: {
-                ":lname": {
-                    "S": Object.values(postBody)[0] //Smith
-                },
-                ":fname": {
-                    "S": Object.values(postBody)[1] //John
-                },
+                //":lname": {
+                //  "S": Object.values(postBody)[0] //Smith
+                //},
+                //":fname": {
+                //  "S": Object.values(postBody)[1] //John
+                //},
             },
-            FilterExpression: "#last_name = :lname AND #first_name = :fname" // lname: { S: 'Smith' }
-        }; 
+            FilterExpression: filterExpString[0]
+        };
 
-        console.log(postBody); //{ lname: 'Smith', fname: 'John' }
+        if (postBody["lname"] !== '') {
+            params["ExpressionAttributeNames"]["#last_name"] = Object.keys(postBody)[0]
+        };
+        if (postBody["fname"] !== '') {
+            params["ExpressionAttributeNames"]["#first_name"] = Object.keys(postBody)[1]
+        };
+        if (postBody["lname"] !== '') {
+            params["ExpressionAttributeValues"][":lname"] = {
+                "S": Object.values(postBody)[0]
+            }
+        };
+        if (postBody["fname"] !== '') {
+            params["ExpressionAttributeValues"][":fname"] = {
+                "S": Object.values(postBody)[1]
+            }
+        };
 
-        console.log(Object.keys(postBody)[0]); //lname
-        console.log(Object.keys(postBody)[1]); //fname
-        console.log(Object.values(postBody)[0]); //Smith
-        console.log(Object.values(postBody)[1]); //John
-
-        console.log(params["ExpressionAttributeValues"][":lname"]); //{ S: 'Smith' }
-        console.log(params["ExpressionAttributeValues"][":fname"]); //{ S: 'John' }
-        console.log(params["ExpressionAttributeValues"]); //{ ':lname': { S: 'Smith' }, ':fname': { S: 'John' } }
-        //to add something to params["ExpressionAttributeValues"] -- params["ExpressionAttributeValues"][":lname"] = {"S": Object.values(postBody)[0]}
-        //thus, if (postBody["lname"] !== void) { params["ExpressionAttributeValues"][":lname"] = {"S": Object.values(postBody)[0]} }
-
-        console.log(params["ExpressionAttributeNames"]["#last_name"]); //lname
-        console.log(params["ExpressionAttributeNames"]["#first_name"]); //fname
-        console.log(params["ExpressionAttributeNames"]); //{ '#last_name': 'lname', '#first_name': 'fname' }
-        console.log(params); //{ '#last_name': 'lname', '#first_name': 'fname' }
+        console.log('params["ExpressionAttributeValues"] = ' + params["ExpressionAttributeValues"]); //{ ':lname': { S: 'Smith' }, ':fname': { S: 'John' } }
+        console.log('params["ExpressionAttributeNames"] = ' + params["ExpressionAttributeNames"]); //{ '#last_name': 'lname', '#first_name': 'fname' }
+        console.log('params["FilterExpression"] = ' + params["FilterExpression"]);
+        console.log('params = ' + params); //{ '#last_name': 'lname', '#first_name': 'fname' }
 
 
         dyn.scan(params, function (err, data) {
@@ -118,78 +124,10 @@ params["ExpressionAttributeNames"] = {}
             //else console.log(data); // successful response --logs entire data object
             console.log(data.Items);
         });
-        /**scan and table******************************************************************************** */
+        /**scan and filter table******************************************************************************** */
     }
 });
 
 
-
-
-
-
-
-
-
-
-
-/*
-app.get('/', (request, response) => response.sendFile(`${path.join(__dirname, '../index-query-post-ddb.html')}`)); //(__dirname, '../) goes one folder up from current location 
-
-app.post('/api/data', (request, response) => {
-    const postBody = request.body;
-
-    //the following puts 'postBody' json into dynamodb database/////////////////////////////////////////
-    //can/should this be made more modular? <--maybe not...
-    var AWS = require('aws-sdk');
-    var dyn = new AWS.DynamoDB({
-        region: 'localhost',
-        endpoint: 'http://localhost:8000',
-        accessKeyId: 'DEFAULT_ACCESS_KEY', // needed if you don't have aws credentials at all in env
-        secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
-    });
-    var params = {
-        TableName: "teamster-application-database",
-        KeyConditionExpression: "ssn = :ssn",
-        ExpressionAttributeValues: {
-            ":ssn": {
-                'S': (data.Items[0].ssn.S)
-            },
-            ":zip": {
-                'S': Object.values(postBody)[0]
-            },
-        },
-    };
-
-    dyn.query(params, function (err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else console.log(data); // successful response --logs entire data object
-        console.log(data.Items); // --logs entire Items object of the data object
-        console.log(data.Items[0]); // --logs 1st index of Items object, which is an array of applicant data objects
-        console.log(data.Items[0].zip); // --logs 'zip' applicant data object, which is an object of this nature: {'S' : 'some string value'}
-        console.log(data.Items[0].zip.S); // --logs the string value of the 'zip' applicant data object. this is what you'll want to provide for the query result.
-        //and so-on, for the rest of the applicant data objects, as shown below:
-        console.log(data.Items[0].union_number.S);
-        console.log(data.Items[0].lname.S);
-        console.log(data.Items[0].fname.S);
-        console.log(data.Items[0].mi.S);
-        console.log(data.Items[0].occupation.S);
-        console.log(data.Items[0].address.S);
-        console.log(data.Items[0].phone.S);
-        console.log(data.Items[0].city.S);
-        console.log(data.Items[0].state.S);
-        console.log(data.Items[0].employer.S);
-        console.log(data.Items[0].employment_date.S);
-        console.log(data.Items[0].employer_address.S);
-        console.log(data.Items[0].employer_phone.S);
-        console.log(data.Items[0].employer_state.S);
-        console.log(data.Items[0].employer_zip.S);
-        console.log(data.Items[0].paid_to.S);
-        console.log(data.Items[0].dob.S);
-        console.log(data.Items[0].membership.S);
-        console.log(data.Items[0].previous_union_number.S);
-        console.log(data.Items[0].ssn.S);
-    });
-});
-*/
 
 app.listen(3000, () => console.info('Application running on port 3000'));
