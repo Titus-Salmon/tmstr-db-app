6733e1ff867c4dcb1aff470ab34c5cb5e76f57f5
tsalm0n <tsalm0n@twc.com>
2019-01-15 17:44:16 -0500

    can now send results of scan & filter back to client (scan-filter.html) via 'response.send(data.Items)'

--- scripts/scan-and-filter-table.js
+++ scripts/scan-and-filter-table.js
@@ -1,14 +1,17 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path'); //node module that provides utilities for working with file and directory paths
+
+
 app.use(bodyParser.urlencoded({ //bodyParser = middleware
     extended: false
 }));
 
-//var categoryFilterArray = [];
-
 app.get('/', (request, response) => response.sendFile(`${path.join(__dirname, '../scan-filter.html')}`)); //(__dirname, '../) goes one folder up from current location 
+//response.sendFile() sends the file ('scan-filter.html') to THIS file (scan-and-filter-table.js), thus we are able to see the
+//scan-filter.html page in our browser when running scan-and-filter-table.js from command line (presumably)
 
 app.post('/api/data', (request, response) => {
     const postBody = request.body;
@@ -16,10 +19,6 @@ app.post('/api/data', (request, response) => {
     console.log(postBody);
     console.log(postBody["lname"]);
     console.log(postBody["fname"]);
-
-    //console.log(Object.values(postBody)[0]);
-
-
 
     /**scan and filter table******************************************************************************** */
     var AWS = require('aws-sdk');
@@ -37,6 +36,7 @@ app.post('/api/data', (request, response) => {
     var lname_exp;
     var fname_exp;
 
+   
     if (postBody["lname"] !== '') { //if data input in html form not empty, push string for FilterExpression to filterExpArray
         lname_exp = "#last_name = :lname";
         filterExpArray.push(lname_exp);
@@ -123,10 +123,14 @@ app.post('/api/data', (request, response) => {
             if (err) console.log(err, err.stack); // an error occurred
             //else console.log(data); // successful response --logs entire data object
             console.log(data.Items);
+            response.send(data.Items); //sends results of scan & filter back to client (scan-filter.html)
         });
         /**scan and filter table******************************************************************************** */
     }
 });
+
+
+
 
 
 
